ead417773a446f2821589aadf34fa0fa8a8a02cb
MaG <Ma-G@ts>
2020-12-30 17:13:03 +0100

    adjust regex to exclude lines with # between other non-white chars

--- pull_not_comments_from_file.sh
+++ pull_not_comments_from_file.sh
@@ -16,9 +16,8 @@ tmp_file=$file.$(date +%H%M%S)
 cd "$path" || true
 cp -p "$file" "$file_bp"
 
-grep -v '^[[:space:]]*$' "$file" > "$tmp_file"
-grep -v '#' "$tmp_file" > "$file"
-rm "$tmp_file"
+grep -v -E '^[[:space:]]*#|^[[:space:]]*$' "$file" > "$tmp_file"
+mv "$tmp_file" "$file"
 
 echo -e "\nNew file $file: \n"
 cat $file
