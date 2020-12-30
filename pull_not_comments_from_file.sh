6c8887b36368543625ba7c2959cb8a8a7c2dd99e
MaG <Ma-G@ts>
2020-12-30 15:45:52 +0100

    śr., 30 gru 2020 15:45:52

---
+++ pull_not_comments_from_file.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+# Takes a target file as an argument
+# strips it from empty lines and comments
+# saves the file with ".bp. + current date and time" prefix 
+# overwrites the original file
+
+
+file=$(echo "$1" | awk -F/ '{print $NF}')
+
+path=$(realpath "$1" | awk -F'/' 'sub(FS $NF,x)')
+file_bp=$file.bp.$(date +%F-%H:%M)
+tmp_file=$file.$(date +%H%M%S)
+
+cd "$path" || exit
+cp -p "$file" "$file_bp"
+
+grep -v '^[[:space:]]*$' "$file" > "$tmp_file"
+grep -v '#' "$tmp_file" > "$file"
+rm "$tmp_file"
+
+echo -e "\nNew file $file: \n"
+cat $file
